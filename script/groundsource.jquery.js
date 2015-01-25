67698d8b99f0d5119300015f95fb280efe78f870
Adam Lutz <amlutz160@gmail.com>
2015-01-25 13:33:08 -0600

    basic working example with heroku-dev api

--- script/groundsource.jquery.js
+++ script/groundsource.jquery.js
@@ -16,31 +16,52 @@
         var groundsource = this;
         this.args = jq_args;
 
-        this.init = function () {
-            if (groundsource.args.hasOwnProperty('settings')) {
-                groundsource.main.mergeSettings(groundsource.args.settings);
-            }
+        this.settings = {
+            api_base_url    : '',   //defaults, will be merged + overridden on init()
+            token           : '',
+            sandbox_mode    : true,
         };
 
-        this.main = {
-            settings : {
-                api_base_url    : '',   //defaults, can be overridden on init()
-                token           : '',
-                sandbox_mode    : true,
+        this.init = function () {
+            if (groundsource.args.hasOwnProperty('settings')) {
+                groundsource.util.mergeSettings(groundsource.args.settings);
+            }
+
+            groundsource.api.loadSurveyResults();
+        };
+
+        this.api = {
+            handleError: function (data) {
+                console.log('error [' + data.status + '] -- ' + data.responseJSON.detail);
             },
+            handleSuccess: function (data) {
+                console.log('success! [' + data.status + ']');
+                console.log(data);
+            },
+            loadSurveyResults : function () {
+                $.ajax({
+                  type: 'GET',
+                  url: groundsource.settings.api_base_url + "/surveys/answeredsurvey/",
+                  data: {},
+                  success : groundsource.api.handleSuccess,
+                  error : groundsource.api.handleError,
+                  beforeSend:function(xhr) {
+                    xhr.setRequestHeader('authorization', 'Bearer ' + groundsource.settings.token);
+                  }
+                });
+            },
+        };
+
+        this.util = {
             mergeSettings : function (settings) {
                 var prop;
                 for (prop in settings) {
                     if (settings.hasOwnProperty(prop)
-                            && groundsource.main.settings.hasOwnProperty(prop)) {
-                        groundsource.main.settings[prop] = settings[prop];
+                            && groundsource.settings.hasOwnProperty(prop)) {
+                        groundsource.settings[prop] = settings[prop];
                     }
                 }
             },
-        };
-
-        this.api = {
-            loadSurveyResults : function () {}
         };
 
         groundsource.init();
